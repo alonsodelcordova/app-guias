966f18b07b86dc052285cdf8739dada25c6e5228
alonsodelcordova <alonsodelcordova@gmail.com>
2020-12-22 10:21:59 -0500

    make it better

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL', default='localhost')
+    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
